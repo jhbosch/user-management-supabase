18faaabb3f261b4d65f3f33335e738862a16b5ba
jhbosch <jesus.hbosch@gmail.com>
2024-03-25 00:57:29 -0400

    svg path animated

--- App.js
+++ App.js
@@ -1,10 +1,35 @@
+import { AnimateSVG } from '@hestabit/react-native-svg-animate';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+
+import { singlePathSVG, sword } from './src/path/Paths'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.container}>
+        <AnimateSVG
+            strokeWidth={8}
+            duration={4000}
+            height={400}
+            scale={1}
+            //width={260}
+            d={sword}
+            viewBox={"0 0 660 660"}
+            type={"async"}
+            animEffect={"bounce"}
+            ref={ref => (this._animateSVG = ref)}
+        />
+
+        <View style={styles.section}>
+            <View style={styles.btn}>
+                <Button title="Replay" onPress={()=> this._animateSVG.replay()} color="red"  />
+            </View>
+            <View style={styles.btn}>
+                <Button title="Rewind" onPress={()=> this._animateSVG.rewind()} color="blue" />
+            </View>
+        </View>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
